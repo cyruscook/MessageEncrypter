568707b05e3ca76555fd62a88d96dc1eba907298
Cyrus Cook <11858105+cyruscook@users.noreply.github.com>
2019-03-11 13:10:14 +0000

    Cleaner solution to directories breaking file structure

--- index.php
+++ index.php
@@ -26,12 +26,14 @@ $dirCount = str_repeat("../", count($_GET));
 <html>
 <head>
 	<title>Encrypted Message</title>
-
+	
+	<base href="<?php echo($dirCount); ?>">
+	
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="<?php echo($dirCount); ?>lib/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T">
+	<link rel="stylesheet" href="lib/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T">
 
 	<!-- Our stylesheet -->
-	<link rel="stylesheet" href="<?php echo($dirCount); ?>style.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 	<div class="container" id="containerBody">
@@ -52,28 +54,28 @@ $dirCount = str_repeat("../", count($_GET));
 	</div>
 
 	<!-- Jquery -->
-	<script src="<?php echo($dirCount); ?>lib/jquery.min.js"></script>
+	<script src="lib/jquery.min.js"></script>
 
 	<!-- Popper -->
-	<script src="<?php echo($dirCount); ?>lib/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="lib/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
 	<!-- Bootstrap -->
-	<script src="<?php echo($dirCount); ?>lib/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="lib/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 	<!-- Bootbox -->
-	<script src="<?php echo($dirCount); ?>lib/bootbox.all.min.js"></script>
+	<script src="lib/bootbox.all.min.js"></script>
 
 	<!-- CryptoJS (https://code.google.com/archive/p/crypto-js/). This is the library with which encryptions will be made -->
 	<!-- AES -->
-	<script src="<?php echo($dirCount); ?>lib/aes.js"></script>
+	<script src="lib/aes.js"></script>
 	<!-- SHA-3 -->
-	<script src="<?php echo($dirCount); ?>lib/sha3.js"></script>
+	<script src="lib/sha3.js"></script>
 	<!-- HMAC SHA-3 -->
-	<script src="<?php echo($dirCount); ?>lib/hmac-sha256.js"></script>
+	<script src="lib/hmac-sha256.js"></script>
 
 
 	<!-- Our Javascript -->
-	<script src="<?php echo($dirCount); ?>script.js"></script>
+	<script src="script.js"></script>
 	<script>
 	createDefaultMessage(ETypeOfRequest.<?php echo($typeOfRequest); ?>);
 	</script>
